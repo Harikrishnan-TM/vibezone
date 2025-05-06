dfedbd9733aa507d748384aa6733d0abfa2a61e7
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-05-06 05:36:00 -0700

    deploying again

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ import dj_database_url  # Make sure you install thiis package
 
 
 
-# Triggering deploy
+# Triggering deploy hkjh
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
