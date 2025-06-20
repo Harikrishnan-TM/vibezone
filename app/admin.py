64e99a37b71e016ee314f116c25b078235e93c5d
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-06-19 20:50:57 -0700

    updating admin.py for call history

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,9 @@ from .models import User, Wallet
 from .models import User, Wallet, WithdrawalTransaction
 
 
+from .models import CallHistory
+
+
 
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'is_online', 'is_staff')
@@ -17,3 +20,9 @@ class WithdrawalTransactionAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Wallet)
+
+
+@admin.register(CallHistory)
+class CallHistoryAdmin(admin.ModelAdmin):
+    list_display = ('caller', 'receiver', 'timestamp')
+    search_fields = ('caller__username', 'receiver__username')
