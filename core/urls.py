bfe4259ea30707ab4f4076082ecf3a553443c79d
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-06-20 00:46:33 -0700

    pushing

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse  # ✅ Add this
+from django.http import JsonResponse  # ✅ Add this ok ok
 
 
 
