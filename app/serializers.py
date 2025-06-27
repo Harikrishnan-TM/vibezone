91263c6cf1fe1c7be42406e5f746e6dd32db8734
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-06-27 15:38:50 -0700

    pushing

--- app/serializers.py
+++ app/serializers.py
@@ -30,7 +30,7 @@ class WalletTransactionSerializer(serializers.ModelSerializer):
         fields = ['type', 'coins', 'description', 'created_at']
 
 
-# serializers.py updating
+# serializers.py updating updating
 
 
 
