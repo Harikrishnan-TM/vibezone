0765a1caf7e08b1c214d3351f8d32d3227473eaf
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-11-07 05:05:20 -0800

    password reset setup

--- core/settings.py
+++ core/settings.py
@@ -248,9 +248,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailersend.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'MS_scWqC1@techzniczone.com'  # Verified sender email
-EMAIL_HOST_PASSWORD = 'mssp.IKieYt8.v69oxl551wdl785k.SZqExRf'  # Use API key as SMTP password
-DEFAULT_FROM_EMAIL = 'support@techzniczone.com'
+ # Use API key as SMTP password
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'support@techzniczone.com')
+
 
 # Email setup
 
