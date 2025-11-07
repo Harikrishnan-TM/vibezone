52fb5dc88dd860837cb15e1f9a0863ab5a8b091f
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-11-07 07:04:33 -0800

    changing password reset setup

--- app/views.py
+++ app/views.py
@@ -11,6 +11,13 @@ from django.db.utils import OperationalError
 from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 
+
+
+
+
+
+from django.conf import settings
+
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 
@@ -1892,3 +1899,24 @@ class CustomPasswordResetConfirmView(auth_views.PasswordResetConfirmView):
 
 class CustomPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
     template_name = 'registration/password_reset_complete.html'
+
+
+
+class CustomPasswordResetView(auth_views.PasswordResetView):
+    template_name = 'registration/password_reset_form.html'
+    email_template_name = 'registration/password_reset_email.html'
+    subject_template_name = 'registration/password_reset_subject.txt'
+    success_url = reverse_lazy('password_reset_done')
+
+    def get_email_context(self, *args, **kwargs):
+        # Get default email context
+        context = super().get_email_context(*args, **kwargs)
+
+        # Build custom frontend reset link
+        uid = context.get('uid')
+        token = context.get('token')
+        context['protocol'] = 'https'
+        context['domain'] = 'vibzeoofficial.in'
+        context['reset_url'] = f"{settings.FRONTEND_RESET_URL}/{uid}/{token}/"
+
+        return context
