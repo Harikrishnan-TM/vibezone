8f56e9069772f113a2195aa734cd4cb74bcf33ae
Harikrishnan-TM <techdatamaxonline@gmail.com>
2025-11-11 04:06:18 -0800

    updating url route

--- app/urls.py
+++ app/urls.py
@@ -150,5 +150,5 @@ urlpatterns = [
     path('password-reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('get-profile/', views.get_profile),  
+    path('api/get-profile/', views.get_profile),
 ]  
